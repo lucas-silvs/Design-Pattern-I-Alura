5c646c9bb43f5580da41d31f2751fddeb6163ca1
Lucas Santos <lucas.parkl@outlook.com>
2022-05-13 12:49:58 -0300

    Desing Patterns I Alura - Implementação do padrão Template Method:
    - problema: foi observado ao aplicar o Design Pattern Chain of Responsability, as classes filhas possuem if's para cada verificação, um para cada desconto existente, ou seja, cada classe de desconto possuia código repetido.
    
    - RESUMO: portanto, foi aplicado o Desing Pattern Template method, onde se possui um processo, seja complexo ou não, mas parte desse processo é repetido entre as classes filhas, e ao invés de repetir esses processos entre as classes filhas, extraimos essas partes para a classe mãe e as partes específicas se mantem na classe filha, implementado através de métodos abstratos implementado pelas classes filhasque serão utilizados pela classe mãe. O objetido desse Design Pattern e remover código duplicado de herança

--- loja/src/br/com/alura/loja/desconto/Desconto.java
+++ loja/src/br/com/alura/loja/desconto/Desconto.java
@@ -9,10 +9,22 @@ public abstract class Desconto {
 
     protected Desconto proximo;
 
+    //Para evitar códigos repetidos do padrão Chain of Responsability
+    // foi aplicado o Design Pattern Template Method
     public Desconto(Desconto proximo) {
         this.proximo = proximo;
     }
 
-    public abstract BigDecimal calcular(Orcamento orcamento);
+    // a estrutura de decisão foi adicionada na classe mãe de Desconto, ou seja, não é necessário mais repetir os if's
+    // ao introduzir os novos descontos
+    //isso foi possivel pois utilizamos a herança do da classe mãe para os descontos existentes
+    // assim as classes filhos apenas tem a sua regra de decisão, e a aplicação do desconto
+    protected abstract BigDecimal efetuarCalculo(Orcamento orcamento);
+    public BigDecimal calcular(Orcamento orcamento){
+        return deveAplicar(orcamento) ? efetuarCalculo(orcamento) : proximo.calcular(orcamento);
+    }
+
+    // foi adicionado um novo método abstrato que é a verificação que cada desconto tem
+    public abstract boolean deveAplicar(Orcamento orcamento);
 
 }
--- loja/src/br/com/alura/loja/desconto/DescontoOrcamentoMaisDeCincoItens.java
+++ loja/src/br/com/alura/loja/desconto/DescontoOrcamentoMaisDeCincoItens.java
@@ -11,10 +11,12 @@ public class DescontoOrcamentoMaisDeCincoItens extends Desconto{
     }
     // Ao verificar se o desconto é aplicavel, se sim é aplicado o desconto e retornado, senão é chamado o próximo desconto
     // assim funciona a dinâmica do Desing Pattern Chain of Responsability
-    public BigDecimal calcular(Orcamento orcamento){
-        if(orcamento.getQuantidadeItens() >5){
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
-        return proximo.calcular(orcamento);
+    public BigDecimal efetuarCalculo(Orcamento orcamento) {
+
+        return orcamento.getValor().multiply(new BigDecimal("0.1"));
+    }
+    @Override
+    public boolean deveAplicar(Orcamento orcamento) {
+        return orcamento.getQuantidadeItens() >5;
     }
 }
